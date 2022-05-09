9a89290875dabee2e6520792de64cd8e9f2caa4e
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-05-09 09:32:51 +0200

    .

--- src/main/kotlin/me/qoomon/examples/Generics.kt
+++ src/main/kotlin/me/qoomon/examples/Generics.kt
@@ -1,17 +1,17 @@
 package me.qoomon.examples
 
 @Suppress("UNCHECKED_CAST")
-fun <SELF> Any.fluent(block: () -> Unit): SELF {
+private inline fun <SELF> Any.self(block: () -> Unit): SELF {
     block()
     return this as SELF
 }
 
 open class A<SELF : A<SELF>> {
-    fun actionA(): SELF = fluent { println("actionA") }
+    fun actionA(): SELF = self { println("actionA") }
 }
 
 open class B<SELF : B<SELF>> : A<SELF>() {
-    fun actionB(): SELF = fluent { println("actionB") }
+    fun actionB(): SELF = self { println("actionB") }
 }
 
 fun main() {
--- src/main/kotlin/me/qoomon/examples/ValueClass.kt
+++ src/main/kotlin/me/qoomon/examples/ValueClass.kt
@@ -3,8 +3,36 @@ package me.qoomon.examples
 @JvmInline
 value class DummyString constructor(val value: String) {
     init {
-        require(value.length <= 8) { "length should be less than 8, but was $value" }
-        require(value == value.lowercase()) { "should be lowercase, but was $value" }
+        require(
+            { require(value.length <= 8) { "length should be less than 8, but was $value" } },
+            { require(value == value.lowercase()) { "should be lowercase, but was $value" } },
+        )
+    }
+}
+
+fun require(vararg requirements: () -> Unit) {
+    val exceptions = requirements.mapNotNull {
+        try {
+            it(); null
+        } catch (ex: IllegalArgumentException) {
+            ex
+        }
+    }
+    if (exceptions.isNotEmpty()) {
+        throw IllegalArgumentException("Failed requirements.").apply {
+            val self = stackTrace[0]
+            // hide require call from stacktrace
+            stackTrace = stackTrace.drop(1).toTypedArray()
+            exceptions.forEach { ex ->
+                ex.apply {
+                    stackTrace = stackTrace.filterNot {
+                        it.className == self.className &&
+                            it.methodName == self.methodName
+                    }.toTypedArray()
+                }
+                addSuppressed(ex)
+            }
+        }
     }
 }
 
